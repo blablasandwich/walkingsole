5afa39909b77895951d750d17273b6615d7572ca
Patrick <35389689+PatAtUCF@users.noreply.github.com>
2018-12-19 16:45:22 -0500

    movmwmet

--- Assets/Scripts/Objects/Floor.cs
+++ Assets/Scripts/Objects/Floor.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class Floor : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject body;
     public GameObject cube;
+    public GameObject point;
+    public GameObject cam;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-     //   player = GameObject.FindGameObjectWithTag("Player");
+       player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     public void test()
     {
-        Debug.Log("test");
-        player.transform.position = new Vector3 (cube.transform.position.x, player.transform.position.y, cube.transform.position.z); 
+        Debug.Log(point.GetComponent<GvrReticlePointer>().ReticleDistanceInMeters);
+
+
+        player.transform.Translate(new Vector3(0, 0, point.GetComponent<GvrReticlePointer>().ReticleDistanceInMeters), body.transform);
+        //player.transform.position = point.GetComponent<GvrReticlePointer>().PointerTransform.position;
+
+        // = player.transform.forward + point.GetComponent<GvrReticlePointer>().ReticleDistanceInMeters;
+
+
     }
 
 
@@ -29,4 +38,5 @@ public class Floor : MonoBehaviour
         {
         Application.Quit();
         }
+
 }
---
+++ Assets/Scripts/PlayerRot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRot : MonoBehaviour
+{
+    public Transform p;
+    GameObject r;
+
+    public GameObject body;
+    public GameObject head;
+
+    float smooth = 0.3f;
+    float distance = 5.0f;
+    float xVelocity = 0.0f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // p = GameObject.FindGameObjectWithTag("Body").transform;
+      //  r = GameObject.Find("GvrReticlePointer");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 lookDir = GvrPointerInputModule.CurrentRaycastResult.worldPosition - p.position;
+        lookDir.y = 0; // keep only the horizontal direction
+        p.rotation = Quaternion.LookRotation(lookDir);
+        //  p.transform.rotation = new Quaternion(r.transform.rotation.w, r.transform.rotation.x, r.transform.rotation.y, r.transform.rotation.z);
+        // p.transform.rotation.
+        // float xAngle = Mathf.SmoothDampAngle(head.transform.eulerAngles.x, body.transform.eulerAngles.x, ref xVelocity, smooth);
+
+        //  Vector3 position = head.transform.position;
+
+        //   position += Quaternion.Euler(0, xAngle, 0) * new Vector3(0, 0, -distance);
+
+        //body.transform.position = position;
+
+    }
+  //  GvrBasePointer.PointerRay
+}
