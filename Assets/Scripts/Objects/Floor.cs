ed794fd9bc8f93670ae86bca6865ac553b083709
Pat <35389689+PatAtUCF@users.noreply.github.com>
2018-12-20 11:45:03 -0500

    walking
    -look at floor to walk
    -look off floor to stop
    -click floor to teliport to location

--- Assets/Scripts/Objects/Floor.cs
+++ Assets/Scripts/Objects/Floor.cs
@@ -6,19 +6,33 @@ public class Floor : MonoBehaviour {
     private Transform body;
     public GameObject player;
     private GvrReticlePointer distance;
-
+    bool walking = false;
+    int MoveRate = 4;
     void Start() {
        player = GameObject.FindGameObjectWithTag("Player");
        body = player.transform.GetChild(2).transform;
        distance = Camera.main.transform.GetChild(0).GetComponent<GvrReticlePointer>();
     }
 
-    public void test() {
+    private void Update()
+    {
+        if (walking)
+        {
+            player.transform.Translate(Vector3.forward * (Time.deltaTime * MoveRate), body);
+        }
+    }
+    public void teleport() {
         player.transform.Translate(new Vector3(0, 0, distance.ReticleDistanceInMeters), body);
+    }
+
+    public void walk()
+    {
+        walking = !walking;
     }
 
     public void exit() {
         Application.Quit();
         }
+
 
 }
